466a8b0689adae42433b71c312cd95ea26832c62
liulinboyi <814921718@qq.com>
2023-08-21 23:33:34 +0800

    feat: 修改打包target

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: "[name].js"
 	},
-	// devtool: 'inline-source-map',
+	devtool: 'inline-source-map',
+    // devtool: 'source-map',
 	devServer: {
 		contentBase: path.join(__dirname, 'dist'),
 		port: 9000,
@@ -34,7 +35,19 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['env'],
+						presets: [
+							[
+								"env", {
+									targets: {
+										chrome: 100,
+										// "browsers": ["last 2 versions", "safari 7"]
+									},
+									modules: false,
+									loose: true
+								},
+							],
+							"stage-1",
+						],
 					}
 				}
 
